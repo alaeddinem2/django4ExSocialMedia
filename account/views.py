d622f3b307bbb9736f92371c1ceb2a6bd9f72fdf
alaeddine <telliala@gmail.com>
2024-03-11 11:45:29 +0100

    add messages

--- account/views.py
+++ account/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .forms import LoginForm, UserRegistrationForm,UserEditForm, ProfileEditForm
 from .models import Profile
 # Create your views here.
@@ -43,6 +44,11 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request,'Profile updated seccessfuly')
+            return redirect('dashboard')
+            
+        else:
+            messages.error(request,"Error updating your profile")
             
             
             
