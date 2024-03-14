fb3ed2cc594d842d34a5166abc330af5f42944f6
alaeddine <telliala@gmail.com>
2024-03-14 07:49:41 +0100

    add bookmarklet

--- project/settings.py
+++ project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'social_django',
     'django_extensions',
+    'images.apps.ImagesConfig',
     
 ]
 
@@ -143,3 +144,21 @@ AUTHENTICATION_BACKENDS = [
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY  = '1051546485446-8ddrbf7h0kf6jq8spum5gn380cvi49g9.apps.googleusercontent.com'
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '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'
 #SOCIAL_AUTH_GOOGLE_SCOPE = ['telliala@gmail.com']
+
+SOCIAL_AUTH_PIPELINE = [
+        'social_core.pipeline.social_auth.social_details',
+        'social_core.pipeline.social_auth.social_uid',
+        'social_core.pipeline.social_auth.auth_allowed',
+        'social_core.pipeline.social_auth.social_user',
+        'social_core.pipeline.user.get_username',
+        'social_core.pipeline.user.create_user',
+        'account.authentication.create_profile',
+        'social_core.pipeline.social_auth.associate_user',
+        'social_core.pipeline.social_auth.load_extra_data',
+        'social_core.pipeline.user.user_details',
+        ]
+
+if DEBUG:
+    import mimetypes
+    mimetypes.add_type('application/javascript', '.js', True)
+    mimetypes.add_type('text/css', '.css', True)
