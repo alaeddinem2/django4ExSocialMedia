b900b75fc3cf01aab0a7b3d84453ed13534d3f8f
alaeddine <telliala@gmail.com>
2024-03-18 09:48:39 +0100

    add toolbar

--- project/settings.py
+++ project/settings.py
@@ -43,10 +43,12 @@ INSTALLED_APPS = [
     'images.apps.ImagesConfig',
     'easy_thumbnails',
     'actions.apps.ActionsConfig',
+    'debug_toolbar',
     
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -171,4 +173,7 @@ ABSOLUTE_URL_OVERRIDES = {
                                          args=[u.username])
 }
 
+INTERNAL_IPS = [
+ '127.0.0.1',
+] 
 #THUMBNAIL_DEBUG = True 
